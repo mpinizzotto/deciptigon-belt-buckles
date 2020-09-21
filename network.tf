04c6f26ff36aaf844f20e1fa3dc0b5c0f0e87e48
mpinizzotto <m.pinizzotto@hotmail.com>
2020-09-21 23:57:40 +0000

    added subnets

--- network.tf
+++ network.tf
@@ -24,6 +24,27 @@ resource aws_internet_gateway "igw-east" {
   }
 }
 
+#create subnet-1 in us-east-1
+resource "aws_subnet" "subnet-east-1" {
+  provider          = aws.region-east
+  availability_zone = element(data.aws_availability_zones.azs-east.names, 0)
+  vpc_id            = aws_vpc.vpc-east.id
+  cidr_block        = "10.216.1.0/24"
+  tags = {
+    Name = "subnet-east-1"
+  }
+}
+
+#create subnet-2 in us-east-1
+resource "aws_subnet" "subnet-east-2" {
+  provider          = aws.region-east
+  availability_zone = element(data.aws_availability_zones.azs-east.names, 1)
+  vpc_id            = aws_vpc.vpc-east.id
+  cidr_block        = "10.216.2.0/24"
+  tags = {
+    Name = "subnet-east-2"
+  }
+}
 
 ##########################################
 
@@ -52,6 +73,18 @@ resource aws_internet_gateway "igw-west" {
   vpc_id   = aws_vpc.vpc-west.id
   tags = {
     Name = "igw-west"
+  }
+}
+
+
+#create subnet in us-west-2
+resource "aws_subnet" "subnet-west-2" {
+  provider          = aws.region-west
+  availability_zone = element(data.aws_availability_zones.azs-west.names, 0)
+  vpc_id            = aws_vpc.vpc-west.id
+  cidr_block        = "10.217.1.0/24"
+  tags = {
+    Name = "subnet-west-1"
   }
 }
 
